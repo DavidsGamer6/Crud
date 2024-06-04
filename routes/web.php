7c3473003f166784c7035cae4dcdd498201d669a
David <bryan.quiroga@dataexpressintmx.com>
2024-06-04 18:45:11 -0500

    Crud Terminado

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Livewire\CreateUser as LivewireCreateUser;
+use App\Livewire\DeleteUser as LivewireDeleteUser;
+use App\Livewire\EditUser as LivewireEditUser;
+
+Route::get('/users/create', LivewireCreateUser::class)->name('users.create');
+Route::get('/users/{user}/edit', LivewireEditUser::class)->name('users.edit');
+Route::get('/users/{user}/delete', LivewireDeleteUser::class)->name('users.delete');
 
 Route::get('/', function () {
     return ['Laravel' => app()->version()];
